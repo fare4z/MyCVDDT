7228b109bd6a9fe90a5a2d2cebfaf9be21890d01
Fareez <fareez@psp.edu.my>
2024-03-20 11:27:42 +0800

    add login activity

---
+++ app/src/main/java/com/fare4z/mycv/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.fare4z.mycv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextView tvRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        tvRegister = findViewById(R.id.tv_register);
+
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
--- app/src/main/java/com/fare4z/mycv/RegisterActivity.java
+++ app/src/main/java/com/fare4z/mycv/RegisterActivity.java
@@ -7,21 +7,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     EditText et_name,et_email,et_nric;
     Button btn_register;
 
+    TextView tvLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
 
         et_name = findViewById(R.id.et_name);
         et_email = findViewById(R.id.et_email);
         et_nric = findViewById(R.id.et_nric);
         btn_register = findViewById(R.id.btn_register);
+        tvLogin = findViewById(R.id.tv_login);
 
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +37,14 @@ public class MainActivity extends AppCompatActivity {
                 bundle.putString("et_nric",et_nric.getText().toString());
 
                 i.putExtras(bundle);
+                startActivity(i);
+            }
+        });
+
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),LoginActivity.class);
                 startActivity(i);
             }
         });
