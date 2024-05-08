77ec71460cc76ae46d48f12098774539c66a1f9d
fareez <fareez@psp.edu.my>
2024-05-09 00:11:28 +0800

    add testing

--- app/src/androidTest/java/com/fare4z/mycv/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/fare4z/mycv/ExampleInstrumentedTest.java
@@ -1,14 +1,26 @@
 package com.fare4z.mycv;
 
-import android.content.Context;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import androidx.test.platform.app.InstrumentationRegistry;
+import static org.junit.Assert.assertEquals;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,10 +29,66 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.fare4z.mycv", appContext.getPackageName());
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() {
+        Intents.init();
     }
+
+    @After
+    public void tearDown() {
+        Intents.release();
+    }
+
+    @Test
+    public void testA_RegisterButton() {
+
+        ActivityScenario<MainActivity> scenario = mActivityRule.getScenario();
+
+        // Type text into EditTexts
+        onView(withId(R.id.et_name)).perform(typeText("Fareez"));
+        onView(withId(R.id.et_email)).perform(typeText("fareez@psp.edu.my"));
+        onView(withId(R.id.et_nric)).perform(typeText("123456789012"));
+
+        // Click on Register Button
+        onView(withId(R.id.btn_register)).perform(click());
+
+        // Check if the Intent to start ProfileActivity is sent
+        intended(hasComponent(ProfileActivity.class.getName()));
+    }
+
+    @Test
+    public void testB_1_NumberButtonClick() {
+        onView(withId(R.id.btn_one)).perform(click());
+        onView(withId(R.id.tvOutputAge)).check(matches(withText("1")));
+    }
+
+    @Test
+    public void testB_2_NumberButtonClick() {
+        onView(withId(R.id.btn_two)).perform(click());
+        onView(withId(R.id.tvOutputAge)).check(matches(withText("2")));
+    }
+
+    @Test
+    public void testB_3_NumberButtonClick() {
+        onView(withId(R.id.btn_three)).perform(click());
+        onView(withId(R.id.tvOutputAge)).check(matches(withText("3")));
+    }
+
+    @Test
+    public void testC_OperationButtonClick() {
+        onView(withId(R.id.btn_plus)).perform(click());
+        onView(withId(R.id.tvOutputAge)).check(matches(withText("+")));
+    }
+
+    @Test
+    public void testD_CalculateMethod() {
+        mActivityRule.getScenario().onActivity(activity -> {
+            int result = activity.kira("1+2");
+            assertEquals(3, result);
+        });
+    }
+
 }
--- app/src/main/java/com/fare4z/mycv/MainActivity.java
+++ app/src/main/java/com/fare4z/mycv/MainActivity.java
@@ -7,11 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText et_name,et_email,et_nric;
     Button btn_register;
+    Button btnOne, btnTwo, btnThree, btnFour, btnFive, btnSix, btnSeven, btnEight, btnNine,btnZero,btnPlus, btnEqual;
+    TextView tvOutputAge;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,33 @@ public class MainActivity extends AppCompatActivity {
         et_email = findViewById(R.id.et_email);
         et_nric = findViewById(R.id.et_nric);
         btn_register = findViewById(R.id.btn_register);
+
+        btnOne = findViewById(R.id.btn_one);
+        btnTwo = findViewById(R.id.btn_two);
+        btnThree = findViewById(R.id.btn_three);
+        btnFour = findViewById(R.id.btn_four);
+        btnFive = findViewById(R.id.btn_five);
+        btnSix = findViewById(R.id.btn_six);
+        btnSeven = findViewById(R.id.btn_seven);
+        btnEight = findViewById(R.id.btn_eight);
+        btnNine = findViewById(R.id.btn_nine);
+        btnZero = findViewById(R.id.btn_zero);
+        btnPlus = findViewById(R.id.btn_plus);
+        btnEqual = findViewById(R.id.btn_equal);
+        tvOutputAge = findViewById(R.id.tvOutputAge);
+
+        btnOne.setOnClickListener(view -> addNumber(1));
+        btnTwo.setOnClickListener(view -> addNumber(2));
+        btnThree.setOnClickListener(view -> addNumber(3));
+        btnFour.setOnClickListener(view -> addNumber(4));
+        btnFive.setOnClickListener(view -> addNumber(5));
+        btnSix.setOnClickListener(view -> addNumber(6));
+        btnSeven.setOnClickListener(view -> addNumber(7));
+        btnEight.setOnClickListener(view -> addNumber(8));
+        btnNine.setOnClickListener(view -> addNumber(9));
+        btnZero.setOnClickListener(view -> addNumber(0));
+        btnPlus.setOnClickListener(view -> operasiTambah());
+
 
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,5 +67,34 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        btnEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int age = kira(tvOutputAge.getText().toString());
+                tvOutputAge.setText(String.valueOf(age));
+            }
+        });
+
     }
+
+    private void addNumber(int number) {
+        String currentText = tvOutputAge.getText().toString();
+        tvOutputAge.setText(currentText + number);
+    }
+
+    private void operasiTambah() {
+        String currentText = tvOutputAge.getText().toString();
+        tvOutputAge.setText(currentText + "+");
+    }
+
+    public int kira(String expression) {
+        String[] parts = expression.split("\\+");
+        int sum = 0;
+        for (String part : parts) {
+            sum += Integer.parseInt(part);
+        }
+        return sum;
+    }
+
 }
